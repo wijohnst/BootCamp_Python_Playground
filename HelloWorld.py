f7557bb194b68fc58f0fe8b38c9fe1156bc4dba9
Will Johnston <wijohnst@gmail.com>
2020-06-26 12:20:07 -0400

    Hello World

---
+++ HelloWorld.py
@@ -0,0 +1,6 @@
+myName = "Will"
+myAge = 33
+myHomeTown = "Columbus, Indiana"
+myIntroString = "Hello World! My name is {} and I am {} years old and I'm from {}."
+
+print(myIntroString.format(myName, myAge, myHomeTown))
